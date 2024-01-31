db786ee935520928b8bac28ce6d550cb239715ab
PauloHGNB <0001038621@senaimgaluno.com.br>
2024-01-31 19:52:03 -0300

    added Atividade 3

---
+++ Atividade 3/atividade1.js
@@ -0,0 +1,18 @@
+let aluno1 = {
+    nome: 'Arlindo',
+    curso: 'Jornalismo',
+};
+
+let aluno2 = {
+    nome: 'Joana',
+    curso: 'Geografia',
+};
+
+let livros = ['As Mentiras de Locke Lamora', 'República de Ladrões', 'Mistborn: O Império Final', 'Mares de Sangue', 'Príncipe dos Espinhos'];
+
+let emprestimo1 = `O Livro "${livros[0]}" está emprestado para ${aluno1.nome} do curso de ${aluno1.curso}`;
+let emprestimo2 = `O Livro "${livros[2]}" está emprestado para ${aluno2.nome} do curso de ${aluno2.curso}`;
+
+// console.log(aluno1);
+console.log(emprestimo1);
+console.log(emprestimo2);

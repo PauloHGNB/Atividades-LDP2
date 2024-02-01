ef13caac6c3060c06f12555638b210d7b4c6046e
PauloHGNB <0001038621@senaimgaluno.com.br>
2024-01-31 21:47:50 -0300

    added new version of Atividade 3

--- Atividade 3/atividade1.js
+++ Atividade 3/atividade1.js
@@ -10,9 +10,5 @@ let aluno2 = {
 
 let livros = ['As Mentiras de Locke Lamora', 'República de Ladrões', 'Mistborn: O Império Final', 'Mares de Sangue', 'Príncipe dos Espinhos'];
 
-let emprestimo1 = `O Livro "${livros[0]}" está emprestado para ${aluno1.nome} do curso de ${aluno1.curso}`;
-let emprestimo2 = `O Livro "${livros[2]}" está emprestado para ${aluno2.nome} do curso de ${aluno2.curso}`;
-
-// console.log(aluno1);
-console.log(emprestimo1);
-console.log(emprestimo2);
+console.log(`O Livro "${livros[0]}" está emprestado para ${aluno1.nome} do curso de ${aluno1.curso}`);
+console.log(`O Livro "${livros[2]}" está emprestado para ${aluno2.nome} do curso de ${aluno2.curso}`);
---
+++ Atividade 3/atividade2.js
@@ -0,0 +1,7 @@
+let escola = { 
+    cursos: ['Matemática', 'Geografia', 'História', 'Direito', 'Biologia', 'Química'],
+    professores: [{nome: 'Lucas', curso: 'Matemática', turno: ['Integral', 'Noturno']}, {nome: 'Regina', curso: 'Química', turno: ['Vespertino', 'Noturno']}],
+    alunos: [{nome: 'Rogério', curso: 'Geografia', idMatricula: 201751073, turno: 'Matutino'}, {nome: 'Samuel', curso: 'Química', idMatricula: 201840001, turno: 'Noturno'}]
+};
+
+console.log(escola);
